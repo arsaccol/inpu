e664877705bd92dc9c8521fe4202b45688fcd124
Alexandre R. Saccol <arsaccol@gmail.com>
2025-01-09 04:16:58 -0300

    add my own made-up transliterations to uniliteral characters

--- characters.js
+++ characters.js
@@ -2,84 +2,111 @@ const characterMapping = {
   uniliterals: [
     { 
       glyph: "𓄿", 
+      transliteration: ";"
     },
     { 
       glyph: "𓇋", 
+      transliteration: "i"
     },
     { 
       glyph: "𓇌", 
+      transliteration: "y"
     },
     { 
       glyph: "𓏭", 
+      transliteration: "y"
     },
     { 
       glyph: "𓂝", 
+      transliteration: "`"
     },
     { 
       glyph: "𓅱", 
+      transliteration: "w"
     },
     { 
       glyph: "𓏲", 
+      transliteration: "w"
     },
     { 
       glyph: "𓃀", 
+      transliteration: "b"
     },
     { 
       glyph: "𓊪", 
+      transliteration: "p"
     },
     { 
       glyph: "𓆑", 
+      transliteration: "f"
     },
     { 
       glyph: "𓅓", 
+      transliteration: "m"
     },
     { 
       glyph: "𓈖", 
+      transliteration: "n"
     },
     { 
       glyph: "𓂋", 
+      transliteration: "r"
     },
     { 
       glyph: "𓉔", 
+      transliteration: "h1"
     },
     { 
       glyph: "𓎛", 
+      transliteration: "h2"
     },
     { 
       glyph: "𓐍", 
+      transliteration: "h3"
     },
     { 
       glyph: "𓄡", 
+      transliteration: "h4"
     },
     { 
-      glyph: "𓋴"
+      glyph: "𓋴",
+      transliteration: "s"
     },
     { 
       glyph: "𓊃", 
+      transliteration: "s"
     },
     { 
       glyph: "𓈙", 
+      transliteration: "sh"
     },
     { 
       glyph: "𓈎", 
+      transliteration: "q"
     },
     { 
       glyph: "𓎡", 
+      transliteration: "k"
     },
     { 
       glyph: "𓎼", 
+      transliteration: "g"
     },
     { 
       glyph: "𓏏", 
+      transliteration: "t"
     },
     { 
       glyph: "𓍿", 
+      transliteration: "ch"
     },
     { 
       glyph: "𓂧", 
+      transliteration: "d"
     },
     { 
       glyph: "𓆓", 
+      transliteration: "dj"
     },
   ]
 
