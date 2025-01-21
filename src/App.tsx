e625f442c78bc13bd55642a42cb2c88be6cdfc69
Alexandre R. Saccol <arsaccol@gmail.com>
2025-01-21 11:01:50 -0300

    show more information about each candidate hieroglyph

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import './App.css'
 import { MaterialIME } from './components/MaterialIME'
+import { Typography } from '@mui/material'
 
 
 function App() {
   return (
     <>
       <div>
-        <h1>𓇋𓈖𓊪𓅱 (pre-α)</h1>
+        <h1>𓇋𓈖𓊪𓅱 (α)</h1>
         Hieroglyph input for the modern scribe.
       </div>
+          <Typography sx={{fontSize: '12px'}}>
+            <a 
+              href="https://forms.gle/iGvfAd5toML9Gxt57" 
+              target="blank"
+
+            >
+              Feedback? Tell us your thoughts!
+            </a>
+        </Typography>
       <MaterialIME/>
-      <a href="https://forms.gle/iGvfAd5toML9Gxt57" target="blank">Have any feedback?</a>
     </>
 
   )
